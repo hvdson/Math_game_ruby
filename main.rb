5b71aad563a628e59b42fc90beca783835e85952
hvdson <hvdsonmusic@gmail.com>
2018-02-27 00:33:49 +0000

    First commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+# ----- CLASSES FOR GAME ----- #
+# calls upon all the classes and uses modules to dictate the game
+# GameState (state)
+  # initialized with player1, player2, is_running?, 
+  
+# Player (state)
+  # initialized with 3 lives
+  # lose a life if wrong answer is incorrect
+  # 
+
+# Turn (state)
+  # keep track of who the current player is
+  # calls question on the current player
+  # 
+
+# Question (behaviour)
+  # generate new question for each turn pick numbers b/t (1..20)
+
+# Scoreboard (state)
+  # display the current score of player 1 and two
